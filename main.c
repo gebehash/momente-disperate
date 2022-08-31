eb2ad10a0f8241fc891aeb71fbc3578ccee52480
gebehash <geo.marsy3@gmail.com>
2022-08-31 10:22:33 +0300

    doamne ajuta

--- main.c
+++ main.c
@@ -7,16 +7,21 @@
 
 int add_last(void **arr, int *len, data_structure *data)
 {
+	printf("%p\n", *arr);
 	int sizeof_data = data->header->len;
-	*arr = realloc(*arr, sizeof_data + sizeof_header);
-	memcpy(*arr+*len, data, sizeof_data + sizeof_header);
-	*len += sizeof_data + sizeof_header;
+	*arr = realloc(*arr, sizeof(*arr) + sizeof_data + sizeof_header);
+	memcpy(*arr, data, sizeof_data + sizeof_header);
+	*len = *len + 1;
+	// for (int i = 0; i <= sizeof(*arr); i++) {
+	// 	printf("%c ", **arr + i);
+	// }
 	return 0;
 }
 
 int add_at(void **arr, int *len, data_structure *data, int index)
 {
 
+	return 0;
 }
 
 void find(void *data_block, int len, int index) 
@@ -27,21 +32,25 @@ void find(void *data_block, int len, int index)
 int delete_at(void **arr, int *len, int index)
 {
 
+	return 0;
 }
 
-data_structure *run_insert(char *buffer) {
-	char *type = strtok(NULL, " ");
+data_structure *run_insert() {
+	printf("inceput");
+	char *typestr = strtok(NULL, " ");
 	char *dedicant = strtok(NULL, " ");
 	char *bancnota1 = strtok(NULL, " ");
 	char *bancnota2 = strtok(NULL, " ");
 	char *dedicat = strtok(NULL, " ");
 	int sizeof_data = 0;
 
-	if (*type == '1') {
+	int type = atoi(typestr);
+
+	if (type == 1) {
 		sizeof_data = strlen(dedicant) + sizeof(int8_t) + sizeof(int8_t) + strlen(dedicat);
-	} else if (*type == '2') {
+	} else if (type == 2) {
 		sizeof_data = strlen(dedicant) + sizeof(int16_t) + sizeof(int32_t) + strlen(dedicat);
-	} else if (*type == '3') {
+	} else if (type == 3) {
 		sizeof_data = strlen(dedicant) + sizeof(int32_t) + sizeof(int32_t) + strlen(dedicat);
 	}
 
@@ -56,36 +65,40 @@ data_structure *run_insert(char *buffer) {
 	memcpy(data->data + strlen(dedicant), bancnota1 -'0', 1);
 	memcpy(data->data + strlen(dedicant) + 1, bancnota2 -'0', 1);
 	memcpy(data->data + strlen(dedicant) + 2, dedicat, strlen(dedicat));
-
+	printf("final");
 	return data;
 } 
 
 int main() {
 	// the vector of bytes u have to work with
 	// good luck :)
-	void *arr = NULL;
+	void *arr = malloc(0);
+	printf("%p\n", arr);
 	int len = 0;
 	char *buffer = (char*) malloc(max_len);
-
+	printf("%p\n", buffer);
+	printf("aici2\n");
 	fgets(buffer, max_len, stdin);
+	// fscanf(stdin, "%s", buffer);
+	printf("%s", buffer);
+	// while (strcmp(buffer, "exit") != 0) {
+	// 	char *command = strtok(buffer, " ");
 
-	while (strcmp(buffer, "exit") != 0) {
-		char *command = strtok(buffer, " ");
+	// 	if (strcmp(command, "insert") == 0) {
+	// 		add_last(arr, &len, run_insert());
+	// 	} else if (strcmp(command, "insert_at") == 0) {
 
-		if (strcmp(command, "insert") == 0) {
-			add_last(arr, len, run_insert(buffer));
-		} else if (strcmp(command, "insert_at") == 0) {
-
-		} else if (strcmp(command, "delete_at") == 0) {
+	// 	} else if (strcmp(command, "delete_at") == 0) {
 			
-		} else if (strcmp(command, "find") == 0) {
+	// 	} else if (strcmp(command, "find") == 0) {
 
-		} else if (strcmp(command, "print") == 0) {
+	// 	} else if (strcmp(command, "print") == 0) {
 
-		}
+	// 	}
 
-		fgets(buffer, max_len, stdin);
-	}
+	// 	// fgets(buffer, max_len, stdin);
+	// 	fscanf(stdin, "%s", buffer);
+	// }
 
 	free(arr);
 
