9b0b1c3d695b92977cccb1ea7058fab0749b9a09
gebehash <geo.marsy3@gmail.com>
2022-09-09 18:47:23 +0300

    get last pos

--- main.c
+++ main.c
@@ -5,19 +5,37 @@
 #define max_len 250
 #define sizeof_header 5
 
+void* get_last_pos(void *arr, int len) {
+	while(len > 0) {
+		int p;
+		memcpy(&p, arr+1, sizeof(int));
+		arr += p;
+		len--;
+	}
+
+	return arr;
+}
+
 int add_last(void **arr, int *len, data_structure *data)
 {
-	printf("%p\n", arr);
-	printf("%p\n", *arr);
-	int sizeof_data = data->header->len;
+	// printf("%p\n", arr);
+	// printf("%p\n", *arr);
+	int sizeof_data = data->header->len - sizeof_header;
 	*arr = realloc(*arr, sizeof(*arr) + sizeof_data + sizeof_header);
-	printf("%p\n", *arr);
-	char *s = (char *)malloc(200);
-	memcpy(s, data->data, sizeof_data);
-	for (int i = 0; i < sizeof_data; i++)
-		printf("%d ", s[i]);
+	// printf("%p\n", *arr);
+	// char *s = (char *)malloc(200);
+	// memcpy(s, data->data, sizeof_data);
+	// for (int i = 0; i < sizeof_data; i++)
+	// 	printf("%d ", s[i]);
 	
-	memcpy(*arr, data, sizeof_data + sizeof_header);
+	// printf("\n");
+	// memcpy(s, data->header, sizeof_header + 5);
+	// for (int i = 0; i < sizeof_header + 5; i++)
+	// 	printf("%d ", s[i]);
+	printf("%p\n", *arr);
+	printf("%p\n",get_last_pos(*arr, *len));
+	memcpy(*arr, data->header, sizeof_header);
+	memcpy(*arr + sizeof_header, data->data, sizeof_data);
 	*len = *len + 1;
 	//printf("date: %s\n", *arr);
 	// for (int i = 0; i <= sizeof(*arr); i++) {
@@ -48,7 +66,7 @@ data_structure *run_insert() {
 	char *dedicant = strtok(NULL, " ");
 	char *bancnota1 = strtok(NULL, " ");
 	char *bancnota2 = strtok(NULL, " ");
-	char *dedicat = strtok(NULL, " ");
+	char *dedicat = strtok(NULL, " \n");
 	int sizeof_data = 0;
 	int b1 = atoi(bancnota1);
 	int b2 = atoi(bancnota2);
@@ -63,17 +81,19 @@ data_structure *run_insert() {
 		sizeof_data = strlen(dedicant) + sizeof(int32_t) + sizeof(int32_t) + strlen(dedicat);
 	}
 
-	data_structure *data = (data_structure *) malloc(sizeof_header);
+	sizeof_data += 2;
+	// printf("sizeof_data: %d\n", sizeof_data);
+	data_structure *data = (data_structure *) malloc(sizeof(data_structure));
 	data->data = malloc(sizeof_data);
 	head *h = (head *) malloc(sizeof_header);
 	h->type = type;
 	h->len = sizeof_header + sizeof_data;
 	data->header = h;
 
-	memcpy(data->data, dedicant, strlen(dedicant));
-	memcpy(data->data + strlen(dedicant), &b1, 4);
-	memcpy(data->data + strlen(dedicant) + 4, &b2, 4);
-	memcpy(data->data + strlen(dedicant) + 8, dedicat, strlen(dedicat));
+	memcpy(data->data, dedicant, strlen(dedicant) + 1);
+	memcpy(data->data + strlen(dedicant) + 1, &b1, 4);
+	memcpy(data->data + strlen(dedicant) + 5, &b2, 4);
+	memcpy(data->data + strlen(dedicant) + 9, dedicat, strlen(dedicat) + 1);
 	return data;
 } 
 
@@ -81,8 +101,8 @@ int main() {
 	// the vector of bytes u have to work with
 	// good luck :)
 	void *arr = malloc(0);
-	printf("%p\n", &arr);
-	printf("%p\n", arr);
+	// printf("%p\n", &arr);
+	// printf("%p\n", arr);
 	int len = 0;
 	char *buffer = (char*) malloc(max_len);
 	fgets(buffer, max_len, stdin);
@@ -91,6 +111,11 @@ int main() {
 		//printf("comanda: %s\n", command);
 		if (strcmp(command, "insert") == 0) {
 			add_last(&arr, &len, run_insert());
+			char *s = (char *)malloc(200);
+			memcpy(s, arr, 70);
+			for (int i = 0; i < 70; i++)
+				printf("%d ", s[i]);
+			printf("\n%d\n", len);
 		} else if (strcmp(command, "insert_at") == 0) {
 
 		} else if (strcmp(command, "delete_at") == 0) {
